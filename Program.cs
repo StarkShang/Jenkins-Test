b1c7f3ee24955da5a367daaf850c40fbaf099401
尚海 <stark.shang@outlook.com>
2018-04-04 23:58:57 +0800

    create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
